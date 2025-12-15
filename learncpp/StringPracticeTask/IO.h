590b56395cf49422a85a430cc8b03688eb75ae03
Muhammad Rafay <123486185+mrafay-dev@users.noreply.github.com>
2025-12-15 00:29:25 +0000

    Create IO.h

---
+++ learncpp/StringPracticeTask/IO.h
@@ -0,0 +1,9 @@
+#ifndef IO_h
+#define IO_h
+#include <string>
+
+std::string getName(int pos);
+int getAge(int pos);
+void printStatement(const std::string& name1, const std::string& name2, int age1, int age2);
+
+#endif
